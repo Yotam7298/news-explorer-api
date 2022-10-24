0ebb896908a6066ce2c4a3a292642525e0c9e304
Yotam7298 <yoyo729800@gmail.com>
2022-10-24 09:48:19 +0300

    connect mongoose

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+const mongoLink = "mongodb://localhost:27017/newsexplorer"
+
+mongoose.connect(mongoLink);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
