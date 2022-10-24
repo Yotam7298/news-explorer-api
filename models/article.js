3f0c79aaa5bf5288fef2f1203a0a7215e9ff9965
Yotam7298 <yoyo729800@gmail.com>
2022-10-24 10:10:39 +0300

    created mongoose user and article models

---
+++ models/article.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const articleSchema = mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "Please provide a URL",
+    },
+  },
+});
+
+module.exports = mongoose.model("article", articleSchema);
